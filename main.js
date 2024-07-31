f7a237bc82ab3642ae169621e3fc6844c4659f2d
abhixdd <noobhacker246@gmail.com>
2024-07-31 22:57:48 +0530

    added main

---
+++ main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+db.defaults({ snippets: [] }).write();
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'renderer.js'),
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    ipcMain.handle('get-snippets', () => {
+        return db.get('snippets').value();
+    });
+
+    ipcMain.handle('add-snippet', (event, snippet) => {
+        db.get('snippets').push(snippet).write();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
